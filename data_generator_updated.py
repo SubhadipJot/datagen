d7760366724549a4338a034dbd074ef752d057fe
Subhadip Jot <jotsubhadip@gmail.com>
2020-06-25 13:55:00 +0530

    Updated Time Manipulation

--- data_generator_updated.py
+++ data_generator_updated.py
@@ -32,7 +32,6 @@ def data_generator(ap_x_origin, ap_y_origin):
     ap_yPos = ap_y_origin
     response_list = list()
     devices = list()
-    last_updated_time_for_mobile_devices = datetime.now()
     # UNIQUE_IDS = (random.sample(range(10000, 999999), 90000))
 
     for i in range(4):  # adding skimmer devices
@@ -53,7 +52,11 @@ def data_generator(ap_x_origin, ap_y_origin):
         device_details['deviceId'] = 'device-' + \
             get_random_alphaNumeric_string()
         device_details['device_type'] = 'mobile'
+        device_details['startBuffer'] = random.choice(random.sample(range(300,7300),1))
+        device_details['timeDelta'] = random.choice(random.sample(range(300,2400),1))
         devices.append(device_details)
+
+    pprint(devices)
 
     for device in devices:
         if (device['device_type'] == 'skimmer'):
@@ -76,15 +79,15 @@ def data_generator(ap_x_origin, ap_y_origin):
                 device_location['yPos'] = device['yPos']
                 device_location['distance'] = round(
                     distance(ap_xPos, ap_yPos, device_location['xPos'], device_location['yPos']))
-                device_location['confidenceFactor'] = 'to be set'
+                device_location['confidenceFactor'] = confidenceFactor(device_location['distance'])
                 device_location['deviceType'] = device['device_type']
                 data['deviceLocationUpdate'] = device_location
                 response_list.append(data)
         else:
-            for i in range(random.choice([600, 900, 1200, 1500, 1800, 2100, 2400])):
+            for i in range(device['timeDelta']):
                 data = {}
                 device_location = {}
-                current_datetime = last_updated_time_for_mobile_devices
+                current_datetime = datetime.now() + timedelta(seconds=device['startBuffer'])
                 incremented_datetime = str(
                     current_datetime + timedelta(seconds=i))
                 datatime_object = datetime.strptime(
@@ -93,7 +96,6 @@ def data_generator(ap_x_origin, ap_y_origin):
                     str(round(random.uniform(10000, 999999)))
                 data['recordTimeStamp'] = int(
                     datatime_object.timestamp() * 1000)
-                last_updated_time_for_mobile_devices = datatime_object
                 data['verboseTime'] = incremented_datetime
                 device_location['macAddress'] = device['macAddress']
                 device_location['deviceId'] = device['deviceId']
@@ -101,7 +103,7 @@ def data_generator(ap_x_origin, ap_y_origin):
                 device_location['yPos'] = random.uniform(2.99, 155.99)
                 device_location['distance'] = round(distance(
                     ap_xPos, ap_yPos, device_location['xPos'], device_location['yPos']))
-                device_location['confidenceFactor'] = 'to be set'
+                device_location['confidenceFactor'] = confidenceFactor(device_location['distance'])
                 device_location['deviceType'] = device['device_type']
                 data['deviceLocationUpdate'] = device_location
                 current_datetime = incremented_datetime
@@ -139,7 +141,7 @@ def data_generator(ap_x_origin, ap_y_origin):
     #         data['deviceLocationUpdate'] = device_location
     #     response_list.append(data)
 
-    with open('data - ' + str(datetime.now()) + '.csv', 'w',) as csvfile:  # generating csv
+    with open('data - ' + '.csv', 'w',) as csvfile:  # generating csv
         writer = csv.writer(csvfile)
         writer.writerow(["recordUid", "recordTimeStamp", "verboseTime",
                          "macAddress", "deviceId", "deviceType", "xPos", "yPos", "distance", "confidenceFactor"])
@@ -159,5 +161,7 @@ def get_random_alphaNumeric_string(stringLength=13):
 def distance(x1, y1, x2, y2):
     return math.sqrt(math.pow(x2 - x1, 2) + math.pow(y2 - y1, 2) * 1.0)
 
+def confidenceFactor(distance):
+    return round((100 - ((distance/235) * 100)))
 
 data_generator(0, 0)  # this is a dummy lat long
