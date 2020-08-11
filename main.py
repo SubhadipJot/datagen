72654b1fa33e1508d5257b80fcc73f881bcb033e
Work <work@Subhadips-MacBook-Air.local>
2020-08-11 16:41:47 +0530

    changed cmdline args

--- main.py
+++ main.py
@@ -8,11 +8,11 @@ import random
 
 if __name__ == '__main__':
     arg_list = sys.argv[1:]
-    if ("type1" in arg_list):
+    if ("1" in arg_list):
         type1skimmer.run()
-    if ("type2" in arg_list):
+    if ("2" in arg_list):
         type2skimmer.run()
-    if ("type3" in arg_list):
+    if ("3" in arg_list):
         type3skimmer.run()
-    if ("mobile" in arg_list):
+    if ("0" in arg_list):
         mobile_devices.run()
